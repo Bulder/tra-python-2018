776d19477cd5f7361d8deba409057796307f4cee
Bulder <jokelaromi@gmail.com>
2018-11-13 02:19:16 +0200

    bad initial navigation, non-functional

--- main.py
+++ main.py
@@ -14,8 +14,31 @@ graph = {}
 for i in range(cityCount):
     graph[i+1] = []
 
-for row in fileRows[1:len(fileRows)-3]: #skip first row and two last rows
+for row in fileRows[1:roadCount+1]:
     rFrom, rTo, rHeight = map(int, row.split(' '))
-    graph[rFrom].append([rTo, rHeight])
+    #add all roads leading out of nodes and setting them to untraversed
+    graph[rFrom].append([rTo, rHeight, False])
+    graph[rTo].append([rFrom, rHeight, False])
 
 print(graph)
+print(str(graph).replace(']],', ']]\n'))
+
+#Beginning of solver
+solved = False
+stack = []
+currentPath = [1]
+highest = 0
+currentNode = 1
+navigated = true
+#traverse graph
+while !solved:
+    if !navigated:
+
+    navigated = false
+    stack.append(graph[currentNode]) #add current node's paths to stack
+    for index, path in enumerate(stack[len(currentPath)-1]):
+        #if the node the target points at only poits at the node we're leaving, or it's already been visited, we just don't for now
+        if len(graph[path[0]]) == 1 or path[1] in currentPath:
+            stack[len(currentPath)-1].pop(index)
+        #TODO we need to figure out how to actually navigate the tree and double back if needed
+        elif highest
