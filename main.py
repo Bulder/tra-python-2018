b0abf3de9a34eef5bc867dac5ffd4a83f7da2044
Bulder <jokelaromi@gmail.com>
2018-11-15 21:24:54 +0200

    Succesfully paths some graphs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import copy
+import time
 
 #prune graph of nodes with only one route
 def pruneDeadEnds(graph, target):
@@ -27,10 +29,63 @@ for i in range(cityCount):
 for row in fileRows[1:roadCount+1]:
     rFrom, rTo, rHeight = map(int, row.split(' '))
     #add all roads leading out of nodes and setting them to untraversed
-    graph[rFrom].append([rTo, rHeight, False])
-    graph[rTo].append([rFrom, rHeight, False])
+    graph[rFrom].append([rTo, rHeight, rFrom])
+    graph[rTo].append([rFrom, rHeight, rTo])
 
 print(graph)
-print(str(graph).replace(']],', ']]\n'))
+#print(str(graph).replace(']],', ']]\n'))
 
-pruneDeadEnds(graph, targetCity)
+absolutePath = [1]
+path = [[1], 0]
+previousPath = [-1]
+availableSteps = []
+previousNode = None
+currentNode = 1
+backtrack = []
+
+while currentNode != targetCity:
+    shortestStep = None
+
+    nextSteps = graph[currentNode].copy()
+
+    #remove new steps going into dead ends or visited nodes
+    for step in nextSteps:
+        if len(graph[step[0]]) <= 1 or step[0] in path[0] or step[0] == previousNode:
+            nextSteps.remove(step)
+
+    #if one of the next steps is shorter or equal to the current highest
+    shortestStep = min(nextSteps, key=lambda x: x[1])
+    while shortestStep[0] == previousPath[-1] or shortestStep[0] == previousNode:
+        print(nextSteps)
+        nextSteps.remove(shortestStep)
+        shortestStep = min(nextSteps, key=lambda x: x[1])
+
+    for step in nextSteps:
+        if step in availableSteps:
+            continue
+        availableSteps += nextSteps
+
+
+    if shortestStep[1] <= path[1]:
+        backtrack = []
+        availableSteps.remove(shortestStep)
+        path[0].append(shortestStep[0])
+        print("hop %d" % shortestStep[0])
+    else:
+        #pick the second best
+        shortestStep = availableSteps.pop(availableSteps.index(min(availableSteps, key=lambda x: x[1])))
+        print(previousNode, currentNode, shortestStep)
+        currentNode = shortestStep[0]
+        previousPath = path[0]
+        path[0] = path[0][:path[0].index(shortestStep[2])+1]
+        path[0].append(shortestStep[0])
+        path[1] = shortestStep[1]
+        print("hop back to %d then hop to %d" % (shortestStep[2], shortestStep[0]))
+
+    previousNode = currentNode
+    currentNode = shortestStep[0]
+    absolutePath.append(currentNode)
+    print(path[0])
+    print(absolutePath, "\n")
+    time.sleep(0.5)
+print(path)
